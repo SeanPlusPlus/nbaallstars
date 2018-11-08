ff2543a4347004ed88d42113f32109d8a585542b
Sean Stephenson <Sean.M.Stephenson@espn.com>
2018-11-08 13:19:25 -0800

    homepage

---
+++ docs/precache-manifest.eefcb99b564d39db23859932eac2d9a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "28a22e69a7a6a9da34d1e533461a0dc0",
+    "url": "./static/media/logo.28a22e69.svg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "8f4a1195f1e237c3dcdc",
+    "url": "./static/js/main.8f4a1195.chunk.js"
+  },
+  {
+    "revision": "cca0da924bce380716d8",
+    "url": "./static/js/1.cca0da92.chunk.js"
+  },
+  {
+    "revision": "8f4a1195f1e237c3dcdc",
+    "url": "./static/css/main.4a7828bf.chunk.css"
+  },
+  {
+    "revision": "cca0da924bce380716d8",
+    "url": "./static/css/1.16c1a535.chunk.css"
+  },
+  {
+    "revision": "55b43180d21ec186e837e45cc32a3fc2",
+    "url": "./index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.55785aeb309cd3465c614f26dccb764f.js"
+  "./precache-manifest.eefcb99b564d39db23859932eac2d9a3.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
