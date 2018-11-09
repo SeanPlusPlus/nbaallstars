29ae2fa3e55252155c62d75b95d443c7ed314452
Sean Stephenson <Sean.M.Stephenson@espn.com>
2018-11-09 09:52:30 -0800

    add api tier

---
+++ api/index.js
@@ -0,0 +1,21 @@
+const serverless = require('serverless-http')
+const express = require('express')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const { NODE_ENV } = process.env
+
+const app = express()
+
+app.get('/api', (req, res) => {
+  const message = 'hello world'
+  res.send({ message })
+})
+
+if (NODE_ENV === 'development') {
+  const port = 3001
+  app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+}
+
+module.exports.handler = serverless(app)
