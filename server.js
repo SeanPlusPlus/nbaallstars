580e1c11e6a4fbafc4aea34c326ef58c4f2446fa
Kane Stapler <kane.stapler@disney.com>
2018-11-11 13:39:31 -0800

    Server endpoint to redirect to twitter

--- server.js
+++ server.js
@@ -2,8 +2,19 @@ const serverless = require('serverless-http')
 const express = require('express')
 const rp = require('request-promise')
 const dotenv = require('dotenv')
+const OAuth = require('oauth')
 
 dotenv.config()
+
+const oauth = new OAuth.OAuth(
+  'https://api.twitter.com/oauth/request_token',
+  'https://api.twitter.com/oauth/access_token',
+  'jJ8BvM2ZVzhy4D0Dj1cOffK8l',
+  'mMuoWNpjUx4Dp1pTSFsODWd352lKDzbJetW9ZdI1WCsjYpFNPc',
+  '1.0A',
+  null,
+  'HMAC-SHA1'
+)
 
 const {
   NODE_ENV,
@@ -28,6 +39,32 @@ app.get('/api', (req, res) => {
       res.send({ message: 'error connecting to api' })
     })
 })
+
+app.get('/twitter/request-token', (req, res) => {
+  res.header({'Access-Control-Allow-Origin': '*'})
+  logInWithTwitter().then((token) => {
+    res.send({token: token})
+  }).catch((error) => {
+    res.status(500)
+    res.send({ message: 'Error getting request token', error: error})
+  })
+})
+
+function logInWithTwitter() {
+  return new Promise((resolve, reject) => {
+    oauth.getOAuthRequestToken({ oauth_callback: 'http://localhost:3000' }, function (error, token, tokenSecret, parsedQueryString) {
+      if (error) {
+        console.log(error)
+        reject(new Error(error))
+      } else {
+        console.log(token)
+        console.log(tokenSecret)
+        console.log(parsedQueryString)
+        resolve(token)
+      }
+    })
+  })
+}
 
 if (NODE_ENV === 'development') {
   const port = 3001
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,24 +1,10 @@
-const OAuth = require('oauth')
-const oauth = new OAuth.OAuth(
-  'http://cors.io/?https://api.twitter.com/oauth/request_token',
-  'https://api.twitter.com/oauth/access_token',
-  'jJ8BvM2ZVzhy4D0Dj1cOffK8l',
-  'mMuoWNpjUx4Dp1pTSFsODWd352lKDzbJetW9ZdI1WCsjYpFNPc',
-  '1.0A',
-  null,
-  'HMAC-SHA1'
-)
-
+const rp = require('request-promise')
 function logInWithTwitter() {
-  oauth.getOAuthRequestToken({ oauth_callback: 'http://localhost:3000' }, function (error, token, tokenSecret, parsedQueryString) {
-    if (error) {
-      console.log(error)
-    } else {
-      console.log(token)
-      console.log(tokenSecret)
-      console.log(parsedQueryString)
-      redirectUserToTwitter(token)
-    }
+  rp.get('http://localhost:3001/twitter/request-token').then((response) => {
+    response = JSON.parse(response)
+    redirectUserToTwitter(response.token)
+  }).catch((error) => {
+    console.log(error)
   })
 }
 
