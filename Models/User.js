35b4460a14d4cb3ac6a4f98faf5151db53c675a9
Sean Stephenson <Sean.M.Stephenson@espn.com>
2018-11-21 10:24:59 -0800

    refactor connection

--- Models/User.js
+++ Models/User.js
@@ -1,44 +1,7 @@
 const Sequelize = require('sequelize')
-const dotenv = require('dotenv')
+const connection = require('./connection')
 
-dotenv.config()
-
-const {
-  NODE_ENV,
-  PG_PASSWORD,
-  PG_USERNAME,
-  PG_DATABASE,
-  PG_HOST,
-} = process.env
-
-const username = PG_USERNAME
-const password = PG_PASSWORD
-const database = PG_DATABASE
-const host = PG_HOST
-
-const { Op } = Sequelize
-
-const sequelize = new Sequelize(database, username, password, {
-  host,
-  port: 5432,
-  logging: console.log, // eslint-disable-line no-console
-  maxConcurrentQueries: 100,
-  dialect: 'postgres',
-  dialectOptions: (NODE_ENV === 'development' ? undefined : { ssl: 'Amazon RDS' }),
-  pool: { maxConnections: 5, maxIdleTime: 30 },
-  language: 'en',
-  operatorsAliases: {
-    $and: Op.and,
-    $or: Op.or,
-    $eq: Op.eq,
-    $gt: Op.gt,
-    $lt: Op.lt,
-    $lte: Op.lte,
-    $like: Op.like,
-  },
-})
-
-const User = sequelize.define('user', {
+const User = connection.define('user', {
   id: { type: Sequelize.STRING, primaryKey: true },
   accessToken: { type: Sequelize.STRING, unique: true },
   accessTokenSecret: { type: Sequelize.STRING, unique: true },
---
+++ Models/connection.js
@@ -0,0 +1,41 @@
+const Sequelize = require('sequelize')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const {
+  NODE_ENV,
+  PG_PASSWORD,
+  PG_USERNAME,
+  PG_DATABASE,
+  PG_HOST,
+} = process.env
+
+const username = PG_USERNAME
+const password = PG_PASSWORD
+const database = PG_DATABASE
+const host = PG_HOST
+
+const { Op } = Sequelize
+
+const connection = new Sequelize(database, username, password, {
+  host,
+  port: 5432,
+  logging: console.log, // eslint-disable-line no-console
+  maxConcurrentQueries: 100,
+  dialect: 'postgres',
+  dialectOptions: (NODE_ENV === 'development' ? undefined : { ssl: 'Amazon RDS' }),
+  pool: { maxConnections: 5, maxIdleTime: 30 },
+  language: 'en',
+  operatorsAliases: {
+    $and: Op.and,
+    $or: Op.or,
+    $eq: Op.eq,
+    $gt: Op.gt,
+    $lt: Op.lt,
+    $lte: Op.lte,
+    $like: Op.like,
+  },
+})
+
+module.exports = connection
