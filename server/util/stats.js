0c9a442bc21caa502e2fc138fffe92f4bbe67a37
Kane Stapler <kane.stapler@disney.com>
2018-12-14 17:06:01 -0800

    Lint stuff

--- server/util/stats.js
+++ server/util/stats.js
@@ -2,12 +2,8 @@ const rp = require('request-promise')
 
 function getPlayerStats(players) {
   const promises = players.map(player => rp.get(player.espnUrl))
-  return Promise.all(promises).then((playerStats) => {
-    console.log(playerStats)
-    return playerStats.map(playerStat => JSON.parse(playerStat))
-  }).catch((error) => {
-    console.log(error)
-  })
+  return Promise.all(promises)
+    .then(playerStats => playerStats.map(playerStat => JSON.parse(playerStat)))
 }
 
 module.exports = {
